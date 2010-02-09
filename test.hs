3d5497c6f933aa8e6744dcee1ce3bee3dfdb332b
Roel van Dijk <vandijk.roel@gmail.com>
2010-02-09 17:02:38 +0100

    More unit tests
    Ignore-this: f33b2b7305113cf763775300bb52157c
    
    darcs-hash:20100209160238-cab80-328d43fc8b6a67a861db311f91acb79d6cded896.gz

--- test.hs
+++ test.hs
@@ -10,7 +10,7 @@ module Main where
 import Control.Applicative ( (<$>) )
 import Control.Concurrent  ( forkIO, threadDelay )
 import Control.Exception   ( catch, try, throwTo, ErrorCall(..), SomeException )
-import Control.Monad       ( (>>=), (>>), return, fail
+import Control.Monad       ( (>>=), (>>), return, fail, fmap
                            , mapM_, replicateM, replicateM_
                            )
 import Data.Bool           ( Bool, not )
@@ -28,9 +28,7 @@ import Data.Function.Unicode ( (∘) )
 import Prelude.Unicode       ( (⋅) )
 
 -- from concurrent-extra
-import           Control.Concurrent.Lock  ( Lock )
 import qualified Control.Concurrent.Lock  as Lock
-import           Control.Concurrent.Event ( Event )
 import qualified Control.Concurrent.Event as Event
 
 -- from HUnit
@@ -53,19 +51,24 @@ main = defaultMain tests
 
 tests ∷ [Test]
 tests = [ testGroup "Events"
-          [ testCase "set wait a"    ∘ assert $ test_event_1 1 1
-          , testCase "set wait b"    ∘ assert $ test_event_1 5 1
-          , testCase "set wait c"    ∘ assert $ test_event_1 1 5
-          , testCase "set wait d"    ∘ assert $ test_event_1 5 5
-          , testCase "conc set wait" ∘ assert $ test_event_2
-          , testCase "multi wake"    ∘ assert $ test_event_3 10
-          , testCase "exception"     ∘ assert $ test_event_4
-          , testCase "wait timeout"  ∘ assert $ test_event_5
+          [ testCase "set wait a"    $ test_event_1 1 1
+          , testCase "set wait b"    $ test_event_1 5 1
+          , testCase "set wait c"    $ test_event_1 1 5
+          , testCase "set wait d"    $ test_event_1 5 5
+          , testCase "conc set wait" $ test_event_2
+          , testCase "multi wake"    $ test_event_3 10
+          , testCase "exception"     $ test_event_4
+          , testCase "wait timeout"  $ test_event_5
+          , testCase "foo!"          $ test_event_6
           ]
         , testGroup "Lock"
-          [ testCase "acquire release" ∘ assert $ test_lock_1
-          , testCase "acquire acquire" ∘ assert $ test_lock_2
-          , testCase "new release"     ∘ assert $ test_lock_3
+          [ testCase "acquire release"    test_lock_1
+          , testCase "acquire acquire"    test_lock_2
+          , testCase "new release"        test_lock_3
+          , testCase "new unlocked"       test_lock_4
+          , testCase "newAcquired locked" test_lock_5
+          , testCase "acq rel unlocked"   test_lock_6
+          , testCase "conc release"       test_lock_7
           ]
         , testGroup "RLock"
           [
@@ -79,91 +82,108 @@ tests = [ testGroup "Events"
 
 -- Set an event 's' times then wait for it 'w' times. This should
 -- terminate within a few moments.
-test_event_1 ∷ Int → Int → IO Bool
-test_event_1 s w = within (10 ⋅ a_moment) $ do
-                     e ← Event.new
-                     replicateM_ s $ Event.set  e
-                     replicateM_ w $ Event.wait e
+test_event_1 ∷ Int → Int → Assertion
+test_event_1 s w = assert $ within (10 ⋅ a_moment) $ do
+  e ← Event.new
+  replicateM_ s $ Event.set  e
+  replicateM_ w $ Event.wait e
 
-test_event_2 ∷ IO Bool
-test_event_2 = within (10 ⋅ a_moment) $ do
-                 e1 ← Event.new
-                 e2 ← Event.new
-                 _ ← forkIO $ helper e1 e2
-                 wait_a_moment
-                 Event.set  e1
-                 Event.wait e2
-  where
-    helper e1 e2 = do
-      Event.wait e1
-      Event.set  e2
+test_event_2 ∷ Assertion
+test_event_2 = assert $ within (10 ⋅ a_moment) $ do
+  e1 ← Event.new
+  e2 ← Event.new
+  _ ← forkIO $ do
+    Event.wait e1
+    Event.set  e2
+  wait_a_moment
+  Event.set  e1
+  Event.wait e2
 
 -- Waking multiple threads with a single Event.
-test_event_3 ∷ Int → IO Bool
-test_event_3 n = within (10 ⋅ a_moment) $ do
-                   e1 ← Event.new
-                   es ← replicateM n $ spawnHelper e1
-                   wait_a_moment
-                   Event.set e1
-                   mapM_ Event.wait es
-  where
-    spawnHelper ∷ Event → IO Event
-    spawnHelper e1 = do
-      e2 ← Event.new
-      _ ← forkIO $ helper e1 e2
-      return e2
-
-    helper e1 e2 = do
+test_event_3 ∷ Int → Assertion
+test_event_3 n = assert $ within (10 ⋅ a_moment) $ do
+  e1 ← Event.new
+  es ← replicateM n $ do
+    e2 ← Event.new
+    _ ← forkIO $ do
       Event.wait e1
       Event.set  e2
+    return e2
+  wait_a_moment
+  Event.set e1
+  mapM_ Event.wait es
 
 -- Exception handling while waiting for an Event.
-test_event_4 ∷ IO Bool
-test_event_4 = within (10 ⋅ a_moment) $ do
-                 e1 ← Event.new
-                 e2 ← Event.new
-                 helperId ← forkIO (helper e1 e2)
-                 wait_a_moment
-                 throwTo helperId $ ErrorCall "Boo!"
-                 Event.wait e2
-  where
-    helper e1 e2 = do
-      let onErr ∷ ErrorCall → IO ()
-          onErr _ = Event.set e2
-      catch (Event.wait e1) onErr
+test_event_4 ∷ Assertion
+test_event_4 = assert $ within (10 ⋅ a_moment) $ do
+  e1 ← Event.new
+  e2 ← Event.new
+  helperId ← forkIO $ catch (Event.wait e1) $ \(_ ∷ ErrorCall) → Event.set e2
+  wait_a_moment
+  throwTo helperId $ ErrorCall "Boo!"
+  Event.wait e2
 
-test_event_5 ∷ IO Bool
-test_event_5 = within (10 ⋅ a_moment) $ do
-                 e ← Event.new
-                 Event.waitTimeout e a_moment
+test_event_5 ∷ Assertion
+test_event_5 = assert $ within (10 ⋅ a_moment) $ do
+  e ← Event.new
+  Event.waitTimeout e a_moment
+
+test_event_6 ∷ Assertion
+test_event_6 = assert $ notWithin (10 ⋅ a_moment) $ do
+  e ← Event.new
+  Event.wait e
 
 
 --------------------------------------------------------------------------------
 -- Locks
 --------------------------------------------------------------------------------
 
-test_lock_1 ∷ IO Bool
-test_lock_1 = within a_moment $ do
-                l ← Lock.new
-                Lock.acquire l
-                Lock.release l
+test_lock_1 ∷ Assertion
+test_lock_1 = assert $ within a_moment $ do
+  l ← Lock.new
+  Lock.acquire l
+  Lock.release l
 
-test_lock_2 ∷ IO Bool
-test_lock_2 = notWithin (10 ⋅ a_moment) $ do
-                l ← Lock.new
-                Lock.acquire l
-                Lock.acquire l
+test_lock_2 ∷ Assertion
+test_lock_2 = assert $ notWithin (10 ⋅ a_moment) $ do
+  l ← Lock.new
+  Lock.acquire l
+  Lock.acquire l
 
 test_lock_3 ∷ Assertion
 test_lock_3 = assertException "" $ do
-                l ← Lock.new
-                Lock.release l
+  l ← Lock.new
+  Lock.release l
+
+test_lock_4 ∷ Assertion
+test_lock_4 = assert $ do
+  l ← Lock.new
+  fmap not $ Lock.locked l
+
+test_lock_5 ∷ Assertion
+test_lock_5 = assert $ do
+  l ← Lock.newAcquired
+  Lock.locked l
+
+test_lock_6 ∷ Assertion
+test_lock_6 = assert $ do
+  l ← Lock.new
+  Lock.acquire l
+  Lock.release l
+  fmap not $ Lock.locked l
+
+test_lock_7 ∷ Assertion
+test_lock_7 = assert ∘ within (10 ⋅ a_moment) $ do
+  l ← Lock.newAcquired
+  _ ← forkIO $ wait_a_moment >> Lock.release l
+  Lock.acquire l
 
 
 --------------------------------------------------------------------------------
 -- RLocks
 --------------------------------------------------------------------------------
 
+-- TODO
 
 -------------------------------------------------------------------------------
 -- Misc
@@ -186,5 +206,5 @@ notWithin t a = not <$> within t a
 assertException ∷ String → IO α → Assertion
 assertException errMsg a = do e ← try a
                               case e of
-                                Left (ex ∷ SomeException ) → return ()
+                                Left (_ ∷ SomeException ) → return ()
                                 Right _ → assertFailure errMsg
