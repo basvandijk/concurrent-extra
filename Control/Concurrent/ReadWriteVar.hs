5baeeaeca3ee745bd89c8487e5f712f70b8f0e9b
Roel van Dijk <vandijk.roel@gmail.com>
2010-02-15 12:39:14 +0100

    ReadWriteVar documentation
    Ignore-this: ed1d635085bf1c62e107237f91a899a3
    
    darcs-hash:20100215113914-cab80-beab4ddd9f2c612a77deade4d9846e81c9e1c204.gz

--- Control/Concurrent/ReadWriteVar.hs
+++ Control/Concurrent/ReadWriteVar.hs
@@ -1,14 +1,50 @@
-{-# LANGUAGE NoImplicitPrelude, TupleSections, UnicodeSyntax #-}
+{-# LANGUAGE CPP, NoImplicitPrelude, TupleSections, UnicodeSyntax #-}
+
+-------------------------------------------------------------------------------
+-- |
+-- Module     : Control.Concurrent.ReadWriteVar
+-- Copyright  : (c) 2010 Bas van Dijk & Roel van Dijk
+-- License    : BSD3 (see the file LICENSE)
+-- Maintainer : Bas van Dijk <v.dijk.bas@gmail.com>
+--            , Roel van Dijk <vandijk.roel@gmail.com>
+--
+-- Concurrent read, sequential write variables. Comparable to an 'IORef' with
+-- more advanced synchronization mechanisms. The value stored inside the 'RWVar'
+-- can be read and used by multiple threads at the same time. Concurrent
+-- computations inside a 'with' \"block\" observe the same value.
+--
+-- Observing and changing the contents of an 'RWVar' are mutually exclusive. The
+-- 'with' function will block if 'modify' is active and vice versa. Furthermore
+-- 'with' is fully sequential and will also block on concurrent calls of 'with'.
+--
+-- Deadlocks:
+--
+-- * @'modify_' v '$' 'const' '$' 'with' v '$' 'const' 'undefined'@
+--
+-- * @'with' v '$' 'const' '$' 'modify_' v '$' 'const' 'undefined'@
+--
+-- * @'modify_' v '$' 'const' '$' 'modify_' v '$' 'const' 'undefined'@
+--
+--
+-- This module is designed to be imported qualified. We suggest importing it
+-- like:
+--
+-- @
+-- import           Control.Concurrent.ReadWriteVar        ( RWVar )
+-- import qualified Control.Concurrent.ReadWriteVar as RWV ( ... )
+-- @
+--
+-------------------------------------------------------------------------------
 
 module Control.Concurrent.ReadWriteVar
   ( RWVar
   , new
   , with
   -- , tryWith
-  , modify
   , modify_
-  , tryModify
+  , modify
   , tryModify_
+  , tryModify
   ) where
 
 
@@ -20,11 +56,15 @@ module Control.Concurrent.ReadWriteVar
 import Control.Monad           ( return, (>>=), return, fail, (>>)
                                , fmap, liftM2
                                )
-import Data.Bool               ( Bool )
+import Data.Bool               ( Bool(..) )
 import Data.Function           ( ($) )
-import Data.Maybe              ( Maybe, isJust )
+import Data.Maybe              ( Maybe(..), isJust )
 import Data.IORef              ( IORef, newIORef, readIORef, writeIORef )
 import System.IO               ( IO )
+#ifdef __HADDOCK__
+import Data.Function           ( const )
+import Prelude                 ( undefined )
+#endif
 
 -- from base-unicode-symbols
 import Data.Function.Unicode   ( (∘) )
@@ -40,26 +80,58 @@ import qualified Control.Concurrent.ReadWriteLock as RWLock
 
 data RWVar α = RWVar RWLock (IORef α)
 
+-- | Create a new 'RWVar'.
 new ∷ α → IO (RWVar α)
 new = liftM2 RWVar RWLock.new ∘ newIORef
 
+{-| Execute an action that operates on the contents of the 'RWVar'.
+
+The action is guaranteed to have a consistent view of the stored value. Any
+function that attempts to 'modify' the contents will block until the action is
+completed.
+
+If another thread is modifying the contents of the 'RWVar' this function will
+block until the other thread finishes its action.
+-}
 with ∷ RWVar α → (α → IO β) → IO β
 with (RWVar l r) f = RWLock.withRead l $ readIORef r >>= f
 
---tryWith ∷ RWVar α → (α → IO β) → IO (Maybe β)
---tryWith (RWVar l r) f = RWLock.tryWithRead l $ readIORef r >>= f
+{-
+tryWith ∷ RWVar α → (α → IO β) → IO (Maybe β)
+tryWith (RWVar l r) f = RWLock.tryWithRead l $ readIORef r >>= f
+-}
 
-modify ∷ RWVar α → (α → IO (α, β)) → IO β
-modify (RWVar l r) = RWLock.withWrite l ∘ modifyIORefM r
+{-| Modify the contents of an 'RWVar'.
 
+This function needs exclusive write access to the 'RWVar'. Only one thread can
+modify an 'RWVar' at the same time. All others will block.
+-}
 modify_ ∷ RWVar α → (α → IO α) → IO ()
 modify_ (RWVar l r) = RWLock.withWrite l ∘ modifyIORefM_ r
 
-tryModify ∷ RWVar α → (α → IO (α, β)) → IO (Maybe β)
-tryModify (RWVar l r) = RWLock.tryWithWrite l ∘ modifyIORefM r
+{-| Modify the contents of an 'RWVar' and return an additional value.
 
+Like 'modify_', but allows a value to be returned (&#x3b2;) in addition to the
+modified value of the 'RWVar'.
+-}
+modify ∷ RWVar α → (α → IO (α, β)) → IO β
+modify (RWVar l r) = RWLock.withWrite l ∘ modifyIORefM r
+
+{-| Attempt to modify the contents of an 'RWVar'.
+
+Like 'modify_', but doesn't block. Returns 'True' if the contents could be
+replaced, 'False' otherwise.
+-}
 tryModify_ ∷ RWVar α → (α → IO α) → IO Bool
 tryModify_ (RWVar l r) = fmap isJust ∘ RWLock.tryWithWrite l ∘ modifyIORefM_ r
+
+{-| Attempt to modify the contents of an 'RWVar' and return an additional value.
+
+Like 'modify', but doesn't block. Returns 'Just' the additional value if the
+contents could be replaced, 'Nothing' otherwise.
+-}
+tryModify ∷ RWVar α → (α → IO (α, β)) → IO (Maybe β)
+tryModify (RWVar l r) = RWLock.tryWithWrite l ∘ modifyIORefM r
 
 
 -------------------------------------------------------------------------------
