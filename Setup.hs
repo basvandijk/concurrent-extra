4da58dd594f4ed1b14286b2d65235324c8123f35
Roel van Dijk <vandijk.roel@gmail.com>
2010-02-15 12:38:24 +0100

    Set __HADDOCK__ flag in haddockHook
    Ignore-this: ebb92d69a2bdf957682b9fd448fe2e88
    
    darcs-hash:20100215113824-cab80-7bc684fb652ec4d3123c99474d560f89a08275c3.gz

--- Setup.hs
+++ Setup.hs
@@ -18,11 +18,13 @@ import System.IO           ( IO )
 -- from cabal
 import Distribution.Simple ( defaultMainWithHooks
                            , simpleUserHooks
-                           , UserHooks(runTests)
+                           , UserHooks(runTests, haddockHook)
                            , Args
                            )
 
 import Distribution.Simple.LocalBuildInfo ( LocalBuildInfo(..) )
+import Distribution.Simple.Program        ( userSpecifyArgs )
+import Distribution.Simple.Setup          ( HaddockFlags )
 import Distribution.PackageDescription    ( PackageDescription(..) )
 
 -------------------------------------------------------------------------------
@@ -30,9 +32,19 @@ import Distribution.PackageDescription    ( PackageDescription(..) )
 main ∷ IO ()
 main = defaultMainWithHooks hooks
   where
-    hooks = simpleUserHooks {runTests = runTests'}
+    hooks = simpleUserHooks
+            { runTests    = runTests'
+            , haddockHook = haddockHook'
+            }
 
 -- Run a 'test' binary that gets built when configured with '-ftest'.
 runTests' ∷ Args → Bool → PackageDescription → LocalBuildInfo → IO ()
 runTests' _ _ _ _ = system testcmd >> return ()
   where testcmd = "." </> "dist" </> "build" </> "test" </> "test"
+
+-- Define __HADDOCK__ for CPP when running haddock.
+haddockHook' ∷ PackageDescription → LocalBuildInfo → UserHooks → HaddockFlags → IO ()
+haddockHook' pkg lbi =
+  haddockHook simpleUserHooks pkg (lbi { withPrograms = p })
+  where
+    p = userSpecifyArgs "haddock" ["--optghc=-D__HADDOCK__"] (withPrograms lbi)
