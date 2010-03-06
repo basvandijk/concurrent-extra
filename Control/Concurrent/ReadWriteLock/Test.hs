84f58b42ac0b0a52fa50e5636b95fb4a56f0aa5a
Bas van Dijk <v.dijk.bas@gmail.com>
2010-03-06 02:56:55 +0100

    Added test1 and test2 to ReadWriteLock.Test
    Ignore-this: d40798c621c1fb49fd85ab68090e5fd0
    Currently they both fail! (I'm working on a fix)
    
    darcs-hash:20100306015655-ae560-2dc89281e3245dc1df400056f2c2bfdfa8cf40fd.gz

--- Control/Concurrent/ReadWriteLock/Test.hs
+++ Control/Concurrent/ReadWriteLock/Test.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude
-           , UnicodeSyntax
-  #-}
+{-# LANGUAGE NoImplicitPrelude , UnicodeSyntax #-}
 
 module Control.Concurrent.ReadWriteLock.Test ( tests ) where
 
@@ -10,23 +8,30 @@ module Control.Concurrent.ReadWriteLock.Test ( tests ) where
 -------------------------------------------------------------------------------
 
 -- from base:
-import Control.Concurrent ( )
+import Control.Monad      ( (>>=), (>>), fail )
+import Control.Concurrent ( forkIO, threadDelay )
+import Data.Function      ( ($) )
+import Prelude            ( fromInteger )
 
 -- from base-unicode-symbols:
-import Prelude.Unicode       ( )
+import Prelude.Unicode    ( (⋅) )
 
 -- from concurrent-extra:
-import qualified Control.Concurrent.ReadWriteLock as RWLock ( )
-import TestUtils ( )
+import qualified Control.Concurrent.ReadWriteLock as RWLock
+    ( new, acquireWrite, acquireRead, releaseWrite, releaseRead  )
+
+import TestUtils ( within, a_moment )
+
+import Utils ( void )
 
 -- from HUnit:
-import Test.HUnit (  )
+import Test.HUnit ( Assertion, assert )
 
 -- from test-framework:
 import Test.Framework  ( Test )
 
 -- from test-framework-hunit:
-import Test.Framework.Providers.HUnit ( )
+import Test.Framework.Providers.HUnit ( testCase )
 
 
 -------------------------------------------------------------------------------
@@ -34,7 +39,53 @@ import Test.Framework.Providers.HUnit ( )
 -------------------------------------------------------------------------------
 
 tests ∷ [Test]
-tests = []
+tests = [ testCase "test1" test1
+        , testCase "test2" test2
+        ]
+
+test1 ∷ Assertion
+test1 = assert $ within (10 ⋅ a_moment) $ do
+          -- Create a new read-write-lock (in the "Free" state):
+          rwl ← RWLock.new
+
+          -- Put the read-write-lock in the "Write" state:
+          RWLock.acquireWrite rwl
+
+          -- Fork a thread that releases the write-lock after a moment:
+          void $ forkIO $ threadDelay a_moment >> RWLock.releaseWrite rwl
+
+          -- This blocks until the write-lock is released in the above thread.
+          RWLock.acquireRead rwl
+
+          -- Release the read-lock so that the read-write-lock can either be
+          -- acquired again by 'acquireRead' or 'acquireWrite':
+          RWLock.releaseRead rwl
+
+          -- The read-write-lock should now be in the "Free" state so the
+          -- following shouldn't deadlock:
+          RWLock.acquireWrite rwl
+
+test2 ∷ Assertion
+test2 = assert $ within (10 ⋅ a_moment) $ do
+          -- Create a new read-write-lock (in the "Free" state):
+          rwl ← RWLock.new
+
+          -- Put the read-write-lock in the "Read" state:
+          RWLock.acquireRead rwl
+
+          -- Fork a thread that releases the read-lock after a moment:
+          void $ forkIO $ threadDelay a_moment >> RWLock.releaseRead rwl
+
+          -- This blocks until the read-lock is released in the above thread.
+          RWLock.acquireWrite rwl
+
+          -- Release the write-lock so that the read-write-lock can either be
+          -- acquired again by 'acquireRead' or 'acquireWrite':
+          RWLock.releaseWrite rwl
+
+          -- The read-write-lock should now be in the "Free" state so the
+          -- following shouldn't deadlock:
+          RWLock.acquireRead rwl
 
 
 -- The End ---------------------------------------------------------------------
