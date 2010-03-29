9f4a0ab6d88909c9e51761bbc2b637ac3329c5bc
Bas van Dijk <v.dijk.bas@gmail.com>
2010-03-29 11:02:19 +0200

    Moved Thread and Thread.Group to their own package 'threads'
    Ignore-this: d2c80d6ddc48a7a6062b7a2d2b5050f6
    
    darcs-hash:20100329090219-ae560-cd8a4f2f5b35b7dacea041d5f1bf99855248e4fb.gz

--- Utils.hs
+++ Utils.hs
@@ -7,17 +7,13 @@ module Utils where
 --------------------------------------------------------------------------------
 
 -- from base:
-import Control.Concurrent.MVar ( MVar, takeMVar, putMVar, tryTakeMVar )
-import Control.Exception       ( SomeException(SomeException)
-                               , blocked, block, unblock
-                               , throwIO
-                               )
+import Control.Concurrent.MVar ( MVar, takeMVar, putMVar )
+import Control.Exception       ( block )
 import Control.Monad           ( Monad, return, (>>=), (>>), fail )
-import Data.Bool               ( Bool(False, True), otherwise )
+import Data.Bool               ( Bool )
 import Data.Function           ( ($) )
 import Data.Functor            ( Functor, (<$) )
 import Data.IORef              ( IORef, readIORef, writeIORef )
-import Data.Maybe              ( Maybe(Nothing, Just) )
 import Prelude                 ( ($!) )
 import System.IO               ( IO )
 
@@ -39,23 +35,8 @@ void = (() <$)
 ifM ∷ Monad m ⇒ m Bool → m α → m α → m α
 ifM c t e = c >>= \b → if b then t else e
 
-anyM ∷ Monad m ⇒ (α → m Bool) → [α] → m Bool
-anyM p = anyM_p
-    where
-      anyM_p []     = return False
-      anyM_p (x:xs) = ifM (p x) (return True) (anyM_p xs)
-
-throwInner ∷ SomeException → IO α
-throwInner (SomeException e) = throwIO e
-
 purelyModifyMVar ∷ MVar α → (α → α) → IO ()
 purelyModifyMVar mv f = block $ takeMVar mv >>= putMVar mv ∘! f
-
-tryRead ∷ MVar α → IO (Maybe α)
-tryRead mv = block $ do mx ← tryTakeMVar mv
-                        case mx of
-                          Nothing → return mx
-                          Just x  → putMVar mv x >> return mx
 
 modifyIORefM ∷ IORef α → (α → IO (α, β)) → IO β
 modifyIORefM r f = do (y, z) ← readIORef r >>= f
@@ -64,31 +45,6 @@ modifyIORefM r f = do (y, z) ← readIORef r >>= f
 
 modifyIORefM_ ∷ IORef α → (α → IO α) → IO ()
 modifyIORefM_ r f = readIORef r >>= f >>= writeIORef r
-
-{-|
-/Strictly/ delete the first element of the list which satisfies the predicate.
-
-This function strictly constructs the list up until the point of the deleted element.
--}
-deleteFirstWhich' ∷ (α → Bool) → [α] → [α]
-deleteFirstWhich' p = deleteFirstWhich'_p
-    where
-      deleteFirstWhich'_p []      = []
-      deleteFirstWhich'_p (x:xs)
-                      | p x       = xs
-                      | otherwise = (x:) $! deleteFirstWhich'_p xs
-
-{-|
-@blockedApply a f@ applies @f@ in a blocked state to @a@ which is executed in
-the blocked state of the current thread.
-
-Handy in case @f@ is a fork which needs to install an exception handler before
-executing @a@.
--}
-blockedApply :: IO α → (IO α → IO β) → IO β
-blockedApply a f = ifM blocked
-                       (f a)
-                       (block $ f $ unblock a)
 
 
 -- The End ---------------------------------------------------------------------
